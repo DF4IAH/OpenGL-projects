39524af79ef0974c160bb83baab62b83e25e8a36
Ulrich Habel <espero7757@gmx.net>
2018-12-19 00:10:59 +0100

    OpenGL_libpq_Draw - (Tutorial12) the Mac seems not to have the GLEW_ARB_debug_output extension?

--- src/OpenGL_libpq_Draw/ogl.cpp
+++ src/OpenGL_libpq_Draw/ogl.cpp
@@ -24,7 +24,6 @@ using namespace glm;
 #include "../common/controls.hpp"
 #include "../common/objloader.hpp"
 #include "../common/vboindexer.hpp"
-#include "../common/text2D.hpp"
 
 #include "ogl.h"
 
@@ -33,6 +32,47 @@ using namespace std;
 
 const int width   = 1024;
 const int height  =  768;
+
+
+// The ARB_debug_output extension, which is used in this tutorial as an example,
+// can call a function of ours with error messages.
+// This function must have this precise prototype ( parameters and return value )
+// See http://www.opengl.org/registry/specs/ARB/debug_output.txt , "New Types" :
+//	The callback function that applications can define, and
+//	is accepted by DebugMessageCallbackARB, is defined as:
+//
+//	    typedef void (APIENTRY *DEBUGPROCARB)(enum source,
+//	                                          enum type,
+//	                                          uint id,
+//	                                          enum severity,
+//	                                          sizei length,
+//	                                          const char* message,
+//	                                          void* userParam);
+void APIENTRY DebugOutputCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam) {
+  printf ("OpenGL Debug Output message : ");
+
+  if      (source == GL_DEBUG_SOURCE_API_ARB)			printf("Source : API; ");
+  else if (source == GL_DEBUG_SOURCE_WINDOW_SYSTEM_ARB)	        printf("Source : WINDOW_SYSTEM; ");
+  else if (source == GL_DEBUG_SOURCE_SHADER_COMPILER_ARB)	printf("Source : SHADER_COMPILER; ");
+  else if (source == GL_DEBUG_SOURCE_THIRD_PARTY_ARB)		printf("Source : THIRD_PARTY; ");
+  else if (source == GL_DEBUG_SOURCE_APPLICATION_ARB)		printf("Source : APPLICATION; ");
+  else if (source == GL_DEBUG_SOURCE_OTHER_ARB)			printf("Source : OTHER; ");
+
+  if      (type == GL_DEBUG_TYPE_ERROR_ARB)			printf("Type : ERROR; ");
+  else if (type == GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR_ARB)	printf("Type : DEPRECATED_BEHAVIOR; ");
+  else if (type == GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR_ARB)        printf("Type : UNDEFINED_BEHAVIOR; ");
+  else if (type == GL_DEBUG_TYPE_PORTABILITY_ARB)		printf("Type : PORTABILITY; ");
+  else if (type == GL_DEBUG_TYPE_PERFORMANCE_ARB)		printf("Type : PERFORMANCE; ");
+  else if (type == GL_DEBUG_TYPE_OTHER_ARB)			printf("Type : OTHER; ");
+
+  if      (severity == GL_DEBUG_SEVERITY_HIGH_ARB)		printf("Severity : HIGH; ");
+  else if (severity == GL_DEBUG_SEVERITY_MEDIUM_ARB)		printf("Severity : MEDIUM; ");
+  else if (severity == GL_DEBUG_SEVERITY_LOW_ARB)		printf("Severity : LOW; ");
+
+  // You can set a breakpoint here ! Your debugger will stop the program,
+  // and the callstack will immediately show you the offending call.
+  printf("Message : %s END.\n", message);
+}
 
 
 ogl::ogl(void)
@@ -53,8 +93,17 @@ ogl::ogl(void)
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL
 
+  // ARB_debug_output is a bit special,
+  // it requires creating the OpenGL context
+  // with paticular flags.
+  // GLFW exposes it this way; if you use SDL, SFML, freeGLUT
+  // or other, check the documentation.
+  // If you use custom code, read the spec :
+  // http://www.opengl.org/registry/specs/ARB/debug_output.txt
+  glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, 1);
+
   // Open a window and create its OpenGL context
-  window = glfwCreateWindow(width, height, "Tutorial 11", nullptr, nullptr);
+  window = glfwCreateWindow(width, height, "Tutorial 12", nullptr, nullptr);
   if (window == nullptr) {
     fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
     glfwTerminate();
@@ -68,6 +117,23 @@ ogl::ogl(void)
     fprintf(stderr, "Failed to initialize GLEW\n");
     glfwTerminate();
     return;
+  }
+
+  // Example 1 :
+  if (GLEW_AMD_seamless_cubemap_per_texture) {
+    printf("The GL_AMD_seamless_cubemap_per_texture is present, (but we're not goint to use it)\n");
+    // Now it's legal to call glTexParameterf with the TEXTURE_CUBE_MAP_SEAMLESS_ARB parameter
+    // You HAVE to test this, because obviously, this code would fail on non-AMD hardware.
+  }
+
+  // Example 2 :
+  if (GLEW_ARB_debug_output) {
+    printf("The OpenGL implementation provides debug output. Let's use it !\n");
+    glDebugMessageCallbackARB(&DebugOutputCallback, NULL);
+    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS_ARB);
+  } else {
+    printf("ARB_debug_output unavailable. You have to use glGetError() and/or gDebugger to catch mistakes.\n");
+    // Please use #define USE_THE_GOOD_ONE
   }
 
 
@@ -85,17 +151,30 @@ ogl::ogl(void)
   // Dark blue background
   glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
+#define USE_THE_GOOD_ONE
+#ifdef USE_THE_GOOD_ONE
   // Enable depth test
-  glEnable(GL_DEPTH_TEST);
+  glEnable(GL_DEPTH_TEST); // SHOULD BE GL_DEPTH_TEST ! WILL TRIGGER AN ERROR MESSAGE !
   // Accept fragment if it closer to the camera than the former one
-  glDepthFunc(GL_LESS);
+  glDepthFunc(GL_LESS);  // SHOULD BE GL_LESS ! WILL TRIGGER AN ERROR MESSAGE !
 
   // Cull triangles which normal is not towards the camera
-  glEnable(GL_CULL_FACE);
+  glEnable(GL_CULL_FACE); // SHOULD BE GL_CULL_FACE ! WILL TRIGGER AN ERROR MESSAGE !
+#else
+  // Enable depth test
+  glEnable(GL_DEPTH); // SHOULD BE GL_DEPTH_TEST ! WILL TRIGGER AN ERROR MESSAGE !
+  // Accept fragment if it closer to the camera than the former one
+  glDepthFunc(GL_LEFT);  // SHOULD BE GL_LESS ! WILL TRIGGER AN ERROR MESSAGE !
 
+  // Cull triangles which normal is not towards the camera
+  glEnable(GL_CULL_FACE_MODE); // SHOULD BE GL_CULL_FACE ! WILL TRIGGER AN ERROR MESSAGE !
+#endif
+
+#if 0
   // Enable blending
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+#endif
 
   GLuint VertexArrayID;
   glGenVertexArrays(1, &VertexArrayID);
@@ -171,9 +250,6 @@ ogl::ogl(void)
   glGenBuffers(1, &elementbuffer);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned short), &indices[0] , GL_STATIC_DRAW);
-
-  // Initialize our little text library with the Holstein font
-  initText2D("Holstein.DDS");
 
 
   // For speed computation
@@ -273,13 +349,10 @@ ogl::ogl(void)
     glDisableVertexAttribArray(1);
     glDisableVertexAttribArray(2);
 
-    char text[256];
-    sprintf(text,"%.2f sec", glfwGetTime());
-    printText2D(text, 10, 500, 60);
+    glfwPollEvents();
 
     // Swap buffers
     glfwSwapBuffers(window);
-    glfwPollEvents();
 
   } while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);  // Check if the ESC key was pressed or the window was closed
 
@@ -292,9 +365,6 @@ ogl::ogl(void)
   glDeleteProgram(programID);
   glDeleteTextures(1, &Texture);
   glDeleteVertexArrays(1, &VertexArrayID);
-
-  // Delete the text's VBO, the shader and the texture
-  cleanupText2D();
 
   // Close OpenGL window and terminate GLFW
   glfwTerminate();
